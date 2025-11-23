ea6bc92547c362efaa443c9e8c855569dd32869b
Erik-sam <kiretoprods@gmail.com>
2025-11-23 18:27:01 +0100

    puzzle1 design and player fix

--- script/player.gd
+++ script/player.gd
@@ -5,6 +5,8 @@ var last_direction := Vector2(1,0)
 var input = Vector2.ZERO
 var direction
 var can_move = true
+enum Direction {NONE, UP, DOWN, RIGHT, LEFT}
+var facing_direction = Direction.NONE
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSSprite2D
 
@@ -25,19 +27,23 @@ func _physics_process(_delta: float) -> void:
 func play_walk_animation():
 	if direction.x > 0:
 		$AnimatedSprite2D.play("right")
+		facing_direction = Direction.RIGHT
 	elif direction.x < 0:
 		$AnimatedSprite2D.play("left")
+		facing_direction = Direction.LEFT
 	elif direction.y > 0:
 		$AnimatedSprite2D.play("down")
+		facing_direction = Direction.DOWN
 	elif direction.y < 0:
 		$AnimatedSprite2D.play("up")
+		facing_direction = Direction.UP
 
 func play_default_animation():
-	if direction.x > 0:
+	if facing_direction == Direction.RIGHT:
 		$AnimatedSprite2D.play("default_right")
-	elif direction.x < 0:
+	elif facing_direction == Direction.LEFT:
 		$AnimatedSprite2D.play("default_left")
-	elif direction.y > 0:
+	elif facing_direction == Direction.DOWN:
 		$AnimatedSprite2D.play("default_down")
-	elif direction.y < 0:
+	elif facing_direction == Direction.UP:
 		$AnimatedSprite2D.play("default_up")
