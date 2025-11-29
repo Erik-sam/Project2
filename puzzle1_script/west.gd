510a18bc4e87e838591eba1093674c90f0f53a3b
Erik-sam <kiretoprods@gmail.com>
2025-11-29 19:10:35 +0100

    visual fix on puzzle1

---
+++ puzzle1_script/west.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+
+@export var is_correct_plate := true
+@onready var area: Area2D = $Area2D
+
+var player_on_plate := false
+
+func _ready():
+	area.body_entered.connect(_on_body_entered)
+	area.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body is Player:
+		player_on_plate = true
+
+func _on_body_exited(body):
+	if body is Player:
+		player_on_plate = false
+
+func _input(event):
+	if player_on_plate and event.is_action_pressed("interact"):
+		interact()
+
+func interact():
+	if is_correct_plate:
+		print("Correct plate chosen!")
+	else:
+		print("Wrong plate (but doing nothing).")
