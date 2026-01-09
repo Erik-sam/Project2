418453880bd0a853527c3e4be11743daff76e83b
pavlakertsadova <pavla.kertsadova@abv.bg>
2026-01-09 12:31:51 +0200

    more menu- settings

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var menu_buttons = $VBoxContainer  # Контейнерът с Start/Exit
+@onready var settings_menu = $SettingsMenu
+
+func _ready():
+	# Уверяваме се, че при старт настройките са скрити
+	settings_menu.visible = false
+	menu_buttons.visible = true
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
@@ -7,3 +14,13 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_settings_button_pressed():
+	# Скриваме главното меню, показваме настройките
+	menu_buttons.visible = false
+	settings_menu.visible = true
+
+func _on_back_button_pressed():
+	# Обратното
+	menu_buttons.visible = true
+	settings_menu.visible = false
